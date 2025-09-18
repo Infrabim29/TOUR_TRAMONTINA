179cd19fb3bd5362d43d711cb97aab0e93036c09
Infrabim29 <infrabim@outlook.com>
2025-09-18 11:26:26 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,181 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-p4_sai02",
+      "name": "P4_SAI02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.1220737047651923,
+        "pitch": 0.8446484395980569,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1434610206688678,
+          "pitch": 0.15831652474310332,
+          "rotation": 3.9269908169872414,
+          "target": "3-p1_ent"
+        },
+        {
+          "yaw": -3.0295605540319794,
+          "pitch": 0.17909963396266093,
+          "rotation": 2.356194490192345,
+          "target": "1-p3_sai01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-p3_sai01",
+      "name": "P3_SAI01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.4515409894396516,
+        "pitch": 0.7602229033977324,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9306787089420663,
+          "pitch": 0.13601426325188726,
+          "rotation": 2.356194490192345,
+          "target": "2-p2_fund"
+        },
+        {
+          "yaw": -1.5909511286124243,
+          "pitch": 0.12571800846014725,
+          "rotation": 3.9269908169872414,
+          "target": "0-p4_sai02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-p2_fund",
+      "name": "P2_FUND",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.3219789796135792,
+        "pitch": 0.8761433384346304,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1235696794000205,
+          "pitch": 0.18366267210990905,
+          "rotation": 2.356194490192345,
+          "target": "3-p1_ent"
+        },
+        {
+          "yaw": -1.3841632578706218,
+          "pitch": 0.2198874410214664,
+          "rotation": 3.9269908169872414,
+          "target": "1-p3_sai01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-p1_ent",
+      "name": "P1_ENT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.1179546136277274,
+        "pitch": 1.3049974892106206,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6211538281152595,
+          "pitch": 0.08885392426217464,
+          "rotation": 3.9269908169872414,
+          "target": "2-p2_fund"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
